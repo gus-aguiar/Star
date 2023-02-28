bb2bde69ca785aae0a2a30ab422f494c37d88272
gus-aguiar <gustavopma90@gmail.com>
2023-02-27 22:20:45 -0300

    requisito 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,87 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import StarContext from '../context/StarContext';
 
 function Table() {
   const { state, search, setSearch } = useContext(StarContext);
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState(0);
+  const [filtered, setFiltered] = useState([]);
+
+  const handleColuna = (event) => {
+    setColumn(event.target.value);
+  };
+
+  const handleComparação = (event) => {
+    setComparison(event.target.value);
+  };
+
+  const handleValueChange = (event) => {
+    setValue(event.target.value);
+  };
 
   const filteredByName = state
     ?.results
     ?.filter((searchRepo) => searchRepo.name.includes(search));
 
+  const handleFilterClick = () => {
+    const filteredByValueAndName = filteredByName.filter((planet) => {
+      if (comparison === 'maior que') {
+        console.log(planet[column]);
+        return Number(planet[column]) > Number(value);
+      }
+      if (comparison === 'menor que') {
+        return Number(planet[column]) < Number(value);
+      }
+      if (comparison === 'igual') {
+        return Number(planet[column]) === Number(value);
+      } return filteredByName;
+    });
+    return setFiltered(filteredByValueAndName);
+  };
+
+  const filterUnion = filtered
+    .filter((planet) => filteredByName
+      .some((p) => p.name === planet
+        .name));
+
   return (
     <div>
+      <div>
+        <label htmlFor="column-filter">Coluna:</label>
+        <select
+          id="column-filter"
+          value={ column }
+          onChange={ handleColuna }
+          data-testid="column-filter"
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <label htmlFor="comparison-filter">comparação</label>
+        <select
+          id="comparison-filter"
+          value={ comparison }
+          onChange={ handleComparação }
+          data-testid="comparison-filter"
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual">igual a</option>
+        </select>
+        <label htmlFor="value-filter">Valor:</label>
+        <input
+          id="value-filter"
+          type="number"
+          value={ value }
+          onChange={ handleValueChange }
+          data-testid="value-filter"
+        />
+        <button onClick={ handleFilterClick } data-testid="button-filter">Filter</button>
+      </div>
       <input
         type="text"
         name="filtros"
@@ -25,15 +97,24 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {filteredByName?.map((planet) => (
-            <tr key={ planet.name }>
-              {Object.values(planet).map((value) => (
-                <td key={ value }>{ value }</td>
-              ))}
-            </tr>
-          ))}
+          {filterUnion.length > 0
+            ? filterUnion?.map((planet) => (
+              <tr key={ planet.name }>
+                {Object.values(planet).map((valor) => (
+                  <td key={ valor }>{ valor }</td>
+                ))}
+              </tr>
+            ))
+            : filteredByName?.map((planet) => (
+              <tr key={ planet.name }>
+                {Object.values(planet).map((valor) => (
+                  <td key={ valor }>{ valor }</td>
+                ))}
+              </tr>
+            ))}
         </tbody>
       </table>
+
     </div>
 
   );
